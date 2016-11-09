442997a230d81a724d48a0d31f7540e087463e80
Gerard Meijer <gerard@meijer.gs>
2016-11-09 10:43:09 +0100

    test

--- src/tds/jenkins/Utilities.groovy
+++ src/tds/jenkins/Utilities.groovy
@@ -1,5 +1,15 @@
 package tds.jenkins
 
-def tdsName() {
-  return 'aap'
+def pullGeneralDockerImages() {
+  docker.image('thedutchselection/postgresql:9.4.8').pull
+}
+
+def setPipelineProperties() {
+  properties(
+    [
+      pipelineTriggers([[$class: 'GitHubPushTrigger']]),
+      [$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator', daysToKeepStr: '30', numToKeepStr: '3']],
+      [$class: 'DisableConcurrentBuildsJobProperty']
+    ]
+  )
 }
