4c9c9680e201336ec71ac20b427811a75e10d514
Gerard Meijer <gerard@meijer.gs>
2016-11-09 10:14:09 +0100

    test

--- src/tds/jenkins/Utilities.groovy
+++ src/tds/jenkins/Utilities.groovy
@@ -2,9 +2,10 @@ package tds.jenkins
 
 class Utilities {
 
-  Utilities() {}
-
-  def tdsName() {
-    return 'aap'
+  def steps
+  Utilities(steps) {this.steps = steps}
+  
+  def mvn(args) {
+    steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
   }
 }
