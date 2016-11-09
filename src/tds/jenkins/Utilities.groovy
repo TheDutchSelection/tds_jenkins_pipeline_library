f060a66a8d544f8f9d9f9f336b83ba6fd3eb99ca
Gerard Meijer <gerard@meijer.gs>
2016-11-09 10:08:18 +0100

    test

--- src/tds/jenkins/Utilities.groovy
+++ src/tds/jenkins/Utilities.groovy
@@ -1,5 +1,3 @@
-#!groovy
-
 package tds.jenkins
 
 class Utilities {
@@ -7,9 +5,4 @@ class Utilities {
   def tdsName() {
     return 'aap'
   }
-
-  // def pullGeneralDockerImages() {
-    // docker.images('thedutchselection/postgresql:9.4.8').pull
-  // }
-
 }
