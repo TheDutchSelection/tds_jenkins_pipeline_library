2a8c70db0246a7d36005cb49916b338794a15eae
Gerard Meijer <gerard@meijer.gs>
2016-11-09 11:40:54 +0100

    added docker container cleanup

--- src/tds/jenkins/Utilities.groovy
+++ src/tds/jenkins/Utilities.groovy
@@ -4,6 +4,10 @@ def cleanupOldDockerImages() {
   sh 'docker rmi $(/usr/bin/docker images -q -f "dangling=true") || true'
 }
 
+cleanupOldDockerTestContainers() {
+  sh 'docker rm -v $(/usr/bin/docker ps -q -f "status=exited" -f "label=test-container") || true'
+}
+
 def pullGeneralDockerImages() {
   pullDockerImage(tdsJenkinsGlobals.elasticsearchImageName, tdsJenkinsGlobals.elasticsearchImageTag)
   pullDockerImage(tdsJenkinsGlobals.postgresqlImageName, tdsJenkinsGlobals.postgresqlImageTag)
