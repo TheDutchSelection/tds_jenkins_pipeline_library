d8001d462370ead5fe81a6fd9d25d6fa338d306e
Gerard Meijer <gerard@meijer.gs>
2019-11-27 09:13:07 +0100

    finished groovy testing

--- src/tds/jenkins/Utilities.groovy
+++ src/tds/jenkins/Utilities.groovy
@@ -3,14 +3,10 @@ package tds.jenkins
 def buildDockerImage(registryAddress, name, tag, args = null) {
   def result = ''
 
-  if (args != null && args.length()>0) {
+  if (args != null && args.length() > 0) {
     result = docker.build(registryAddress + '/' + name + ':' + tag, args)
-    println "with args"
-    println result
   } else {
     result = docker.build(registryAddress + '/' + name + ':' + tag)
-    println "without args"
-    println result
   }
 
   return result
